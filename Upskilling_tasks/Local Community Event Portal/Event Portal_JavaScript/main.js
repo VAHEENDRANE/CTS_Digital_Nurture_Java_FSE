3462743acf1fc471cd112a670ac6365f73b74876
VAHEENDRANE <rvaheendrane@gmail.com>
2026-06-03 23:27:20 +0530

    Completed JavaScript Exercise 2 - Data Types and Operators

--- Upskilling_tasks/Local Community Event Portal/Event Portal_JavaScript/main.js
+++ Upskilling_tasks/Local Community Event Portal/Event Portal_JavaScript/main.js
@@ -5,3 +5,37 @@ window.onload = function () {
     alert("Page Fully Loaded");
 
 };
+
+
+// Event Information
+
+const eventName = "Music Festival";
+
+const eventDate = "15-06-2026";
+
+let availableSeats = 50;
+
+
+// Template Literal
+
+console.log(
+    `Event: ${eventName} | Date: ${eventDate} | Seats: ${availableSeats}`
+);
+
+
+// Registration
+
+availableSeats--;
+
+console.log(
+    `After Registration, Available Seats: ${availableSeats}`
+);
+
+
+// Cancellation
+
+availableSeats++;
+
+console.log(
+    `After Cancellation, Available Seats: ${availableSeats}`
+);
